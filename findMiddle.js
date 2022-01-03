67fc7e217d81667199b6e3958f301edd2ff40297
Jani Kuhno <25201137+janikuhno@users.noreply.github.com>
2022-01-03 15:47:33 +0200

    Adjusted naming

---
+++ findMiddle.js
@@ -0,0 +1,13 @@
+const findMiddle = (linkedList) => {
+  let fast = linkedList.head;
+  let slow = linkedList.head;
+
+  while (fast !== null) {
+    fast = fast.getNextNode();
+    if (fast !== null) {
+      fast = fast.getNextNode();
+      slow = slow.getNextNode();
+    }
+  }
+  return slow;
+};
---
+++ nthLastNode.js
@@ -0,0 +1,18 @@
+const LinkedList = require("./LinkedList");
+
+const nthLastNode = (linkedList, n) => {
+  let current = null;
+  let tailSeeker = linkedList.head;
+  let count = 0;
+  while (tailSeeker) {
+    tailSeeker = tailSeeker.next;
+    if (count >= n) {
+      if (!current) {
+        current = linkedList.head;
+      }
+      current = current.next;
+    }
+    count++;
+  }
+  return current;
+};
